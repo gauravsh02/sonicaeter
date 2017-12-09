c384d5d96787c4dff0f0be1518780962e95ac8fb
nawaj07 <im_nawaj@outlook.com>
2017-12-09 19:49:39 +0530

    hamburger icon update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
       $(window).scroll(function() {
-        if ($(document).scrollTop() > 100) { 
+        if ($(document).scrollTop() > 100) {
           $(".navbar").css("background-color", "#fff");
           $(".navbar").css("box-shadow", "rgb(165, 169, 173) 0px 0px 20px 2px");
           $(".navbar-brand").css("color", "#000");
@@ -13,3 +13,15 @@ $(document).ready(function(){
         }
       });
     });
+
+    $(document).ready(function(){
+          $(window).scroll(function() {
+            if ($(document).scrollTop() > 100) {
+              $(".navbar-toggler").css("border-color", "#000");
+              $(".navbar-toggler-icon").css("background-image", 'url("./assets/download.svg")');
+            } else {
+                $(".navbar-toggler").css("border-color", "#fff");
+                $(".navbar-toggler-icon").css("background-image", 'url("./assets/download 1.svg")');
+            }
+          });
+        });
